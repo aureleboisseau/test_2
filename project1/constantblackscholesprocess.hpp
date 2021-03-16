1b0c1d2e28db4e1825567c9060a3dc556a4943ff
aureleboisseau <72968705+aureleboisseau@users.noreply.github.com>
2021-03-16 10:16:23 +0100

    test_BSP
    we have our dividend yeld, risk freerate, and volatility constant as wanted in the statement

--- project1/constantblackscholesprocess.hpp
+++ project1/constantblackscholesprocess.hpp
@@ -5,7 +5,48 @@ namespace QuantLib {
 
     class ConstantBlackScholesProcess : public StochasticProcess1D {
 
-        // your implementation goes here
+        GeneralizedBlackScholesProcess(Handle<Quote> x0,
+                                        Handle<YieldTermStructure> dividendTS,
+                                        Handle<YieldTermStructure> riskFreeTS,
+                                        Handle<BlackVolTermStructure> blackVolTS,
+                                        const ext::shared_ptr<discretization>& d =
+                                            ext::shared_ptr<discretization>(new EulerDiscretization),
+                                        bool forceDiscretization = false);
+  
+         GeneralizedBlackScholesProcess(Handle<Quote> x0,
+                                        Handle<YieldTermStructure> dividendTS,
+                                        Handle<YieldTermStructure> riskFreeTS,
+                                        Handle<BlackVolTermStructure> blackVolTS,
+                                        Handle<LocalVolTermStructure> localVolTS);
+  
+  
+         Real x0() const override;
+         Real drift(Time t, Real x) const override;
+         Real diffusion(Time t, Real x) const override;
+         Real apply(Real x0, Real dx) const override;
+         Real expectation(Time t0, Real x0, Time dt) const override;
+         Real stdDeviation(Time t0, Real x0, Time dt) const override;
+         Real variance(Time t0, Real x0, Time dt) const override;
+         Real evolve(Time t0, Real x0, Time dt, Real dw) const override;
+         Time time(const Date&) const override;
+  
+         void update() override;
+  
+         const Handle<Quote>& stateVariable() const;
+         const Handle<YieldTermStructure>& dividendYield() const;
+         const Handle<YieldTermStructure>& riskFreeRate() const;
+         const Handle<BlackVolTermStructure>& blackVolatility() const;
+         const Handle<LocalVolTermStructure>& localVolatility() const;
+       private:
+         Handle<Quote> x0_;
+         Handle<YieldTermStructure> riskFreeRate_, dividendYield_;
+         Handle<BlackVolTermStructure> blackVolatility_;
+         Handle<LocalVolTermStructure> externalLocalVolTS_;
+         bool forceDiscretization_;
+         bool hasExternalLocalVol_;
+         mutable RelinkableHandle<LocalVolTermStructure> localVolatility_;
+         mutable bool updated_, isStrikeIndependent_;
+  
         
     };
 
