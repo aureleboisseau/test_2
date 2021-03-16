0532b269b14347bdf67d0085dff4bfd50f0c179d
aureleboisseau <72968705+aureleboisseau@users.noreply.github.com>
2021-03-16 10:36:06 +0100

    test-3

--- project1/constantblackscholesprocess.hpp
+++ project1/constantblackscholesprocess.hpp
@@ -9,7 +9,7 @@ namespace QuantLib {
         GeneralizedBlackScholesProcess(
             const Handle<Quote>& x0,
             const Handle<YieldTermStructure>& dividendTS,
-            const Handle<YieldTermStructure>& riskFreeTS,
+            const Handle<YieldTermStructure>& riskFreeRate,
             const Handle<BlackVolTermStructure>& blackVolTS,
             const boost::shared_ptr<discretization>& d =
                                 boost::shared_ptr<discretization>());
