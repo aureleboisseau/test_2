338d533fc7924f26babf6fb4ae9f768e04082689
aureleboisseau <72968705+aureleboisseau@users.noreply.github.com>
2021-03-17 11:18:37 +0100

    volatility_fixed

--- project1/main.cpp
+++ project1/main.cpp
@@ -47,9 +47,9 @@ int main() {
         Handle<YieldTermStructure> riskFreeRate(
             ext::shared_ptr<YieldTermStructure>(
                 new ZeroCurve({today, today + 6*Months}, {0.015,0.015}, dayCounter)));
-        Handle<BlackVolTermStructure> volatility(
-            ext::shared_ptr<BlackVolTermStructure>(
-                new BlackVarianceCurve(today, {today+1*Months, today+2*Months,today+3*Months, today+4*Months,today+5*Months, today+6*Months}, {0.05,0.10,0.15,0.20,0.25,0.50}, dayCounter)));
+       
+
+        Volatility volatility = ql.LocalConstantVol(today, volatilityH, dayCounter);
       
       
 
