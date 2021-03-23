e0c9b6a90b6b832d864e2363236ba288cd15e777
aureleboisseau <72968705+aureleboisseau@users.noreply.github.com>
2021-03-23 15:29:57 +0100

    ee1

--- project1/main.cpp
+++ project1/main.cpp
@@ -1,44 +1,77 @@
+#include <ql/qldefines.hpp>
+#ifdef BOOST_MSVC
+#  include <ql/auto_link.hpp>
+#endif
+#include "constantblackscholesprocess.hpp"
+#include "mceuropeanengine.hpp"
+#include <ql/instruments/vanillaoption.hpp>
+#include <ql/instruments/payoffs.hpp>
+#include <ql/exercise.hpp>
+#include <ql/pricingengines/vanilla/mceuropeanengine.hpp>
+#include <ql/termstructures/yield/zerocurve.hpp>
+#include <ql/termstructures/volatility/equityfx/blackvariancecurve.hpp>
+#include <ql/time/calendars/target.hpp>
+#include <ql/utilities/dataformatters.hpp>
 #include <iostream>
-#include <cstdlib>
-
-#include <boost/test/unit_test.hpp>
-#include <boost/detail/lightweight_test.hpp>
-#include <ql/quantlib.hpp>
-#include <boost/format.hpp>
-
-
+#include <chrono>
 
 using namespace QuantLib;
+
 int main() {
 
     try {
-        Real strike = 40.0;
+
+        // modify the sample code below to suit your project
+
+        Calendar calendar = TARGET();
+        Date today = Date(24, February, 2021);
+        Settings::instance().evaluationDate() = today;
+
+        Option::Type type(Option::Put);
+        Real underlying = 36;
+        Real strike = 40;
+        Date maturity(24, May, 2021);
+
+        ext::shared_ptr<Exercise> europeanExercise(new EuropeanExercise(maturity));
+        ext::shared_ptr<StrikedTypePayoff> payoff(new PlainVanillaPayoff(type, strike));
+      
         Real timeToMaturity = .5; //years
-        Real spot = 36.0;
+       
         Rate riskFree = .03;
-        Rate dividendYield = 0.1;
+        Rate dividendYield = 0.0;
         Volatility sigma = .20;
-
-        //QuantLib requires sigma * sqrt(T) rather than just sigma/volatility
         Real vol = sigma * std::sqrt(timeToMaturity);
-
-        //calculate dividend discount factor assuming continuous compounding (e^-rt)
         DiscountFactor growth = std::exp(-dividendYield * timeToMaturity);
-
-        //calculate payoff discount factor assuming continuous compounding 
         DiscountFactor discount = std::exp(-riskFree * timeToMaturity);
+        VanillaOption europeanOption(payoff, europeanExercise);
+        BlackScholesCalculator bsCalculator(payoff, underlying, growth, vol, discount);
+        Size timeSteps = 10;
+        Size mcSeed = 42;
+        ext::shared_ptr<PricingEngine> mcengine;
+        mcengine = MakeMCEuropeanEngine_2<PseudoRandom>(bsCalculator)
+            .withSteps(timeSteps)
+            .withAbsoluteTolerance(0.01)
+            .withSeed(mcSeed);
+        europeanOption.setPricingEngine(mcengine);
 
-        //instantiate payoff function for a call 
-        boost::shared_ptr<PlainVanillaPayoff> vanillaCallPayoff = 
-            boost::shared_ptr<PlainVanillaPayoff>(new PlainVanillaPayoff(Option::Type::Call, strike));
+        auto startTime = std::chrono::steady_clock::now();
 
-        BlackScholesCalculator bsCalculator(vanillaCallPayoff, spot, growth, vol, discount);
-        Real Value = bsCalculator.value();
-        std::cout <<"Value:"<< Value << std::endl;
+        Real NPV = europeanOption.NPV();
+        Real you = bsCalculator.value();
+        auto endTime = std::chrono::steady_clock::now();
 
+        double us = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count();
+        
+        std::cout << "NPV: " << you << std::endl;
+        std::cout << "Elapsed time: " << us / 1000000 << " s" << std::endl;
 
         return 0;
 
-    
+    } catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    } catch (...) {
+        std::cerr << "unknown error" << std::endl;
+        return 1;
     }
 }
