0cd9549a118b2ff3fd97f71bd0a41310085cfa20
aureleboisseau <72968705+aureleboisseau@users.noreply.github.com>
2021-03-23 15:19:51 +0100

    ff13

--- project1/main.cpp
+++ project1/main.cpp
@@ -6,35 +6,44 @@
 #include <ql/quantlib.hpp>
 #include <boost/format.hpp>
 
-namespace {
+
 
 using namespace QuantLib;
+int main() {
+
+    try {
+        Real strike = 110.0;
+        Real timeToMaturity = .5; //years
+        Real spot = 100.0;
+        Rate riskFree = .03;
+        Rate dividendYield = 0.0;
+        Volatility sigma = .20;
+
+        //QuantLib requires sigma * sqrt(T) rather than just sigma/volatility
+        Real vol = sigma * std::sqrt(timeToMaturity);
+
+        //calculate dividend discount factor assuming continuous compounding (e^-rt)
+        DiscountFactor growth = std::exp(-dividendYield * timeToMaturity);
+
+        //calculate payoff discount factor assuming continuous compounding 
+        DiscountFactor discount = std::exp(-riskFree * timeToMaturity);
+
+        //instantiate payoff function for a call 
+        boost::shared_ptr<PlainVanillaPayoff> vanillaCallPayoff = 
+            boost::shared_ptr<PlainVanillaPayoff>(new PlainVanillaPayoff(Option::Type::Call, strike));
+
+        BlackScholesCalculator bsCalculator(vanillaCallPayoff, spot, growth, vol, discount);
+        Real Value = bsCalculator.value();
+        std::cout <<"Value:"<< Value << std::endl;
 
 
+        return 0;
 
-Real strike = 110.0;
-Real timeToMaturity = .5; //years
-Real spot = 100.0;
-Rate riskFree = .03;
-Rate dividendYield = 0.0;
-Volatility sigma = .20;
-
-//QuantLib requires sigma * sqrt(T) rather than just sigma/volatility
-Real vol = sigma * std::sqrt(timeToMaturity);
-
-//calculate dividend discount factor assuming continuous compounding (e^-rt)
-DiscountFactor growth = std::exp(-dividendYield * timeToMaturity);
-
-//calculate payoff discount factor assuming continuous compounding 
-DiscountFactor discount = std::exp(-riskFree * timeToMaturity);
-
-//instantiate payoff function for a call 
-boost::shared_ptr<PlainVanillaPayoff> vanillaCallPayoff = 
-    boost::shared_ptr<PlainVanillaPayoff>(new PlainVanillaPayoff(Option::Type::Call, strike));
-
-BlackScholesCalculator bsCalculator(vanillaCallPayoff, spot, growth, vol, discount);
-Real Value = bsCalculator.value();
-std::cout <<"Value:"<< Value << std::endl;
-
-    
+    } catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    } catch (...) {
+        std::cerr << "unknown error" << std::endl;
+        return 1;
+    }
 }
