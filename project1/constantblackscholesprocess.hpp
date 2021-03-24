616b1ea1daea554aa1abb778d5eae6db04b9b912
aureleboisseau <72968705+aureleboisseau@users.noreply.github.com>
2021-03-24 12:27:12 +0100

    Update constantblackscholesprocess.hpp

--- project1/constantblackscholesprocess.hpp
+++ project1/constantblackscholesprocess.hpp
@@ -12,69 +12,42 @@
  namespace QuantLib {
   
      class LocalConstantVol;
-    
-  
-  
-     class GeneralizedBlackScholesProcess2 : public StochasticProcess1D {
-       public:
-         GeneralizedBlackScholesProcess2(const Real x0,
-                                         const Real dividendTS,
-                                         const Real riskFreeTS,
-                                         const Real blackVolTS,
-                                         const ext::shared_ptr<discretization>& d =
-                                             ext::shared_ptr<discretization>(new EulerDiscretization),
-                                         bool forceDiscretization = false);
-  
-         GeneralizedBlackScholesProcess2(const Real x0,
-                                        const Real dividendTS,
-                                        const Real riskFreeTS,
-                                        const Real blackVolTS,
-                                       const Real localVolTS);
-  
-  
-         Real x0() const override;
-         Real drift(Time t, Real x) const override;
-         Real diffusion(Time t, Real x) const override;
-         Real apply(Real x0, Real dx) const override;
-         Real expectation(Time t0, Real x0, Time dt) const override;
-         Real stdDeviation(Time t0, Real x0, Time dt) const override;
-         Real variance(Time t0, Real x0, Time dt) const override;
-         Real evolve(Time t0, Real x0, Time dt, Real dw) const override;
-         Time time(const Date&) const override;
-  
-         void update() override;
-  
-         const Handle<Quote>& stateVariable() const;
-         const Handle<YieldTermStructure>& dividendYield() const;
-         const Handle<YieldTermStructure>& riskFreeRate() const;
-         const Handle<BlackVolTermStructure>& blackVolatility() const;
-         const Handle<LocalVolTermStructure>& localVolatility() const;
-       private:
-         Handle<Quote> x0_;
-         Handle<YieldTermStructure> riskFreeRate_, dividendYield_;
-         Handle<BlackVolTermStructure> blackVolatility_;
-         Handle<LocalVolTermStructure> externalLocalVolTS_;
-         bool forceDiscretization_;
-         bool hasExternalLocalVol_;
-         mutable RelinkableHandle<LocalVolTermStructure> localVolatility_;
-         mutable bool updated_, isStrikeIndependent_;
-     };
-  
-  
-     class BlackScholesProcess2 : public GeneralizedBlackScholesProcess2 {
-       public:
-         BlackScholesProcess2(
-             const Real x0,
-             const Real riskFreeTS,
-             const Real blackVolTS,
-             const ext::shared_ptr<discretization>& d =
-                   ext::shared_ptr<discretization>(new EulerDiscretization),
-             bool forceDiscretization = false);
-     };
-  
 
-  
- }
-  
+      class GeneralizedBlackScholesProcess : public StochasticProcess1D {
+      public:
+        GeneralizedBlackScholesProcess(
+            const Handle<Quote>& x0,
+            const Handle<YieldTermStructure>& dividendTS,
+            const Handle<YieldTermStructure>& riskFreeTS,
+            const Handle<BlackVolTermStructure>& blackVolTS,
+            const boost::shared_ptr<discretization>& d =
+                                boost::shared_ptr<discretization>());
+        Real x0() const {
+            return x0_->value();
+        }
+        Real drift(Time t, Real x) const {
+            Real sigma = diffusion(t,x);
+            Time t1 = t + 0.0001;
+            return riskFreeRate_->forwardRate(t,t1,Continuous,...)
+                 - dividendYield_->forwardRate(t,t1,Continuous,...)
+                 - 0.5 * sigma * sigma;
+        }
+        Real diffusion(Time t, Real x) const;
+        Real apply(Real x0, Real dx) const {
+            return x0 * std::exp(dx);
+        }
+        Real expectation(Time t0, Real x0, Time dt) const {
+            QL_FAIL("not implemented");
+        }
+        Real evolve(Time t0, Real x0, Time dt, Real dw) const {
+            return apply(x0, discretization_->drift(*this,t0,x0,dt) +
+                             stdDeviation(t0,x0,dt)*dw);
+        }
+        const Handle<Quote>& stateVariable() const;
+        const Handle<YieldTermStructure>& dividendYield() const;
+        const Handle<YieldTermStructure>& riskFreeRate() const;
+        const Handle<BlackVolTermStructure>& blackVolatility() const;
+        const Handle<LocalVolTermStructure>& localVolatility() const;
+    };
   
  #endif
